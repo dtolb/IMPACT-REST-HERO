689d7f804c1d3cf6a3cbc82f4c95724df3b29397
Dan Tolbert <dantolbe@cisco.com>
2023-08-29 10:38:35 -0700

    update app.py to create a pet too

--- Sample_SDKs/python-client-generated/app.py
+++ Sample_SDKs/python-client-generated/app.py
@@ -4,33 +4,32 @@ import swagger_client
 from swagger_client.rest import ApiException
 from pprint import pprint
 
-# Configure API key authorization: api_key
 configuration = swagger_client.Configuration()
 configuration.api_key['api_key'] = 'abc123456'
-# Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
-# configuration.api_key_prefix['api_key'] = 'Bearer'
 
-# create an instance of the API class
-api_instance = swagger_client.ReadApi(swagger_client.ApiClient(configuration))
-id = 1 # float | id of the pet to fetch
+read_api = swagger_client.ReadApi(swagger_client.ApiClient(configuration))
+write_api = swagger_client.WriteApi(swagger_client.ApiClient(configuration))
+id = 1
+
+YOUR_PETS_NAME="FILL_THIS_OUT"
 
 try:
-    api_response = api_instance.get_pet(id)
+    createPetRequest = swagger_client.models.CreatePetRequest(name=YOUR_PETS_NAME, species="dog")
+    api_response = write_api.create_pet(createPetRequest)
+    pprint(api_response)
+    id = api_response.id
+except ApiException as e:
+    print("Exception when calling WriteAPI->create_pet: %s\n" % e)
+
+try:
+    api_response = read_api.get_pet(id)
     pprint(api_response)
 except ApiException as e:
     print("Exception when calling ReadApi->get_pet: %s\n" % e)
 
-# Configure API key authorization: api_key
-configuration = swagger_client.Configuration()
-configuration.api_key['api_key'] = 'abc123456'
-# Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
-# configuration.api_key_prefix['api_key'] = 'Bearer'
-
-# create an instance of the API class
-api_instance = swagger_client.ReadApi(swagger_client.ApiClient(configuration))
-
 try:
-    api_response = api_instance.list_pets()
+    api_response = read_api.list_pets()
     pprint(api_response)
 except ApiException as e:
     print("Exception when calling ReadApi->list_pets: %s\n" % e)
+
