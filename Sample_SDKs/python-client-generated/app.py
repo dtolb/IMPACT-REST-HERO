60b19198ee440ffae6a1cd6ee1379379930def00
Dan Tolbert <dantolbe@cisco.com>
2023-08-29 10:51:54 -0700

    better app.py

--- Sample_SDKs/python-client-generated/app.py
+++ Sample_SDKs/python-client-generated/app.py
@@ -11,11 +11,13 @@ read_api = swagger_client.ReadApi(swagger_client.ApiClient(configuration))
 write_api = swagger_client.WriteApi(swagger_client.ApiClient(configuration))
 id = 1
 
-YOUR_PETS_NAME="FILL_THIS_OUT"
+YOUR_PETS_NAME="Dolph"
+YOUR_UPDATED_PETS_NAME="Molly"
 
 try:
     createPetRequest = swagger_client.models.CreatePetRequest(name=YOUR_PETS_NAME, species="dog")
     api_response = write_api.create_pet(createPetRequest)
+    print("--Created Pet--")
     pprint(api_response)
     id = api_response.id
 except ApiException as e:
@@ -23,12 +25,22 @@ except ApiException as e:
 
 try:
     api_response = read_api.get_pet(id)
+    print("--Get Pet--")
     pprint(api_response)
 except ApiException as e:
     print("Exception when calling ReadApi->get_pet: %s\n" % e)
 
 try:
+    updatePetRequest = swagger_client.models.UpdatePetRequest(name=YOUR_UPDATED_PETS_NAME)
+    api_response = write_api.update_pet(id=id, body=updatePetRequest)
+    print("--Update Pet--")
+    pprint(api_response)
+except ApiException as e:
+    print("Exception when calling WriteAPI->update_pet: %s\n" % e)
+
+try:
     api_response = read_api.list_pets()
+    print("--List Pets--")
     pprint(api_response)
 except ApiException as e:
     print("Exception when calling ReadApi->list_pets: %s\n" % e)
